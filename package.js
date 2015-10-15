221fe9b5c4da44de3d956e54e31251651f0b17ef
Brent Abrahams <brent_abrahams@yahoo.com>
2015-10-15 22:27:41 +0800

    Updated dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'constellation:subscriptions',
-  version: '0.3.2',
+  version: '0.3.3',
   summary: 'Subscriptions plugin for Constellation',
   git: 'https://github.com/JackAdams/constellation-subscriptions.git',
   documentation: 'README.md',
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1');
 
   api.use(['templating','session','blaze','reactive-dict'], 'client');
-  api.use('constellation:console@1.2.2', 'client');
+  api.use('constellation:console@1.2.3', 'client');
 
   api.addFiles('subscriptions.css','client');
   api.addFiles('subscriptions.html','client');
